f6208c94bd70766d628fbc02805dda4fbb2ac396
Sergio Montes <sergiomontes@MacBook-Pro-de-Sergio.local>
2020-03-23 17:13:01 -0600

    initial commit

---
+++ StateHealth/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StateHealth
+//
+//  Created by Sergio Montes on 3/23/20.
+//  Copyright © 2020 myadtech.mx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ StateHealthTests/StateHealthTests.swift
@@ -0,0 +1,34 @@
+//
+//  StateHealthTests.swift
+//  StateHealthTests
+//
+//  Created by Sergio Montes on 3/23/20.
+//  Copyright © 2020 myadtech.mx. All rights reserved.
+//
+
+import XCTest
+@testable import StateHealth
+
+class StateHealthTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
